10b3297e9e422bec4cbee52ac408f9a56c906857
edissonchamorroc <john.chamorro@udea.edu.co>
2022-08-16 20:55:31 -0500

    comentario de prueba

---
+++ src/main/java/chamorro/edisson/contabilidad/ContabilidadEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package chamorro.edisson.contabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadEmpresarialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chamorro/edisson/contabilidad/ContabilidadEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package chamorro.edisson.contabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
