a82f725889a5343f3fd636e61ce44f7525936c2b
edissonchamorroc <john.chamorro@udea.edu.co>
2022-09-23 21:00:31 -0500

    Se eliminan archivos no usados y repetidos

--- src/main/java/chamorro/edisson/contabilidad/repositories/PerfilRepositorio.java
+++
@@ -1,7 +0,0 @@
-package chamorro.edisson.contabilidad.repositories;
-
-import chamorro.edisson.contabilidad.models.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilRepositorio extends JpaRepository<Perfil,Long> {
-}
