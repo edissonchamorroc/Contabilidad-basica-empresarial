f8e0e8ef949c457e26254c981f40aab679f5998b
edissonchamorroc <john.chamorro@udea.edu.co>
2023-02-09 16:13:53 -0500

    se elimina todo el proyecto para empezar de cero y con mejoras

--- src/main/java/chamorro/edisson/contabilidad/repositories/DineroRepositorio.java
+++
@@ -1,7 +0,0 @@
-package chamorro.edisson.contabilidad.repositories;
-
-import chamorro.edisson.contabilidad.models.MovimientoDinero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DineroRepositorio extends JpaRepository<MovimientoDinero,Long> {
-}
--- src/main/java/chamorro/edisson/contabilidad/repositories/EmpleadoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package chamorro.edisson.contabilidad.repositories;
-
-import chamorro.edisson.contabilidad.models.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface EmpleadoRepositorio extends JpaRepository<Empleado,Long> {
-    public Empleado findByEmail(String email);
-}
