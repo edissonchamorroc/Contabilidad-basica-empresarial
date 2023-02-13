ddddbdbb3fa0760bc1a60a00112f20ac624c559c
edissonchamorroc <john.chamorro@udea.edu.co>
2023-02-13 18:08:16 -0500

    agrega funcionalidad login

--- src/main/java/com/springmvc/contabilidad/security/SecurityConfiguration.java
+++ src/main/java/com/springmvc/contabilidad/security/SecurityConfiguration.java
@@ -6,18 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration  {
+public class SecurityConfiguration {
 
     @Autowired
     MyUserDetailsService userDetailsService;
@@ -47,7 +45,7 @@ public class SecurityConfiguration  {
 
         http.authenticationProvider(daoAuthenticationProvider());
 
-         http
+        http
                 //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests((auth) -> auth
                         .antMatchers("/registro-empleado", "/employee", "/styles/**", "/js/**", "/vendor/**").permitAll()
@@ -56,7 +54,7 @@ public class SecurityConfiguration  {
                 )
                 .formLogin((login) -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/home",true)
+                        .defaultSuccessUrl("/home", true)
                         .permitAll()
                 )
                 .logout((logout) -> logout
@@ -72,40 +70,4 @@ public class SecurityConfiguration  {
         return http.build();
     }
 
-   /*@Bean
-   public DaoAuthenticationProvider authProvider() {
-       DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       authProvider.setUserDetailsService(userDetailsService);
-       authProvider.setPasswordEncoder(passwordEncoder());
-       return authProvider;
-   }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests((auth) -> auth
-                        .antMatchers("/registro-empleado", "/employee", "/styles/**", "/js/**", "/vendor/**").permitAll()
-                        .anyRequest().authenticated()
-
-                )
-                .formLogin((login) -> login
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home",true)
-                        .permitAll()
-                )
-                .logout((logout) -> logout
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/login")
-                        .permitAll()
-                )
-        ;
-    }*/
 }
